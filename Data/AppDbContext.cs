7bfea3e04fa846361467bca77e3627300cfd8274
LDVP5049 <luismaster752@gmail.com>
2023-10-30 13:02:00 -0700

    Cambios

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApi_ASA.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext()
+        {
+            
+        }
+    }
+}
