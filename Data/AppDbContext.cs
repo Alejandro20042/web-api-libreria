f898ae0d2264fdc1a8ddc02492d41275ca921b4f
LDVP5049 <luismaster752@gmail.com>
2023-10-30 13:06:51 -0700

    Cambios 2

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,12 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using WebApi_ASA.Data.Models;
 
 namespace WebApi_ASA.Data
 {
-    public class AppDbContext:DbContext
+    public class AppDbContext : DbContext
     {
-        public AppDbContext()
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
             
         }
+        
+        public DbSet<Book> Books { get; set; }
     }
 }
